31d3a28ecffe7d6393f1cbeafddb70b32c9309f3
NusratNawshin <nawshinnusrat@gmail.com>
2022-12-09 16:22:56 -0500

    Updated BERT and T5

--- sarcasm_detection_BERT.py
+++ sarcasm_detection_BERT.py
@@ -1,5 +1,4 @@
 
-
 # from google.colab import drive
 # import sys
 #
@@ -20,7 +19,7 @@ data_v2 = pd.read_json(r'Sarcasm_Headlines_Dataset_v2.json', lines = True)
 print(data.shape)
 print(data_v2.shape)
 
-final_data = pd.concat([data,data_v2] , ignore_index = True , axis =0)
+final_data = pd.concat([data ,data_v2] , ignore_index = True , axis =0)
 print(final_data.shape)
 final_data.info()
 
@@ -29,7 +28,13 @@ df = pd.DataFrame()
 df['text'] = final_data['headline']
 df['label'] = final_data['is_sarcastic']
 
-df.groupby(['label']).size().plot.bar()
+# df['text'] = final_data['headline'][:500]
+# df['label'] = final_data['is_sarcastic'][:500]
+
+
+# df.groupby(['label']).size().plot.bar()
+
+
 
 # # pre processing
 import re
@@ -38,14 +43,17 @@ import nltk
 from nltk.corpus import stopwords
 
 # remove numbers
+print("Removing Numbers from text")
 df['text'] = [re.sub(r'[^a-zA-Z]', ' ', str(sentence)) for sentence in df['text']]
 print(df.head())
 
 # lower the characters
+print("Lowering Characters")
 df['text'] = [sentence.lower() for sentence in df['text']]
 print(df.head())
 
-#remove stopwords
+# remove stopwords
+print("Removing Stopwords from text")
 list =[]
 for sentence in df['text']:
     list.append([i for i in str(sentence).split() if i not in (stopwords.words('english'))])
@@ -54,6 +62,7 @@ df['text'] = [' '.join(l) for l in list]
 print(df.head())
 
 # lemmatization
+print("Lemmatizing text")
 wnl = nltk.WordNetLemmatizer()
 list = []
 for sentence in df['text']:
@@ -62,25 +71,25 @@ for sentence in df['text']:
 df['text'] = list
 print(df.head())
 
-
 import torch
 import numpy as np
 from transformers import BertTokenizer
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
-labels = {0:0,
-          1:1
+labels = {0: 0,
+          1: 1
           }
+
+
 # labels = [0,1]
 
 class Dataset(torch.utils.data.Dataset):
 
     def __init__(self, df):
-
         self.labels = [labels[label] for label in df['label']]
         # self.labels = labels
-        self.texts = [tokenizer(text, 
-                               padding='max_length', max_length = 256, truncation=True,
+        self.texts = [tokenizer(text,
+                                padding='max_length', max_length=256, truncation=True,
                                 return_tensors="pt") for text in df['text']]
 
     def classes(self):
@@ -98,46 +107,51 @@ class Dataset(torch.utils.data.Dataset):
         return self.texts[idx]
 
     def __getitem__(self, idx):
-
         batch_texts = self.get_batch_texts(idx)
         batch_y = self.get_batch_labels(idx)
 
         return batch_texts, batch_y
 
-np.random.seed(112)
-df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42), 
-                                     [int(.8*len(df)), int(.9*len(df))])
 
-print(len(df_train),len(df_val), len(df_test))
+np.random.seed(112)
+df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42),
+                                     [int(.8 * len(df)), int(.9 * len(df))])
+
+print(len(df_train), len(df_val), len(df_test))
 
 from torch import nn
 from transformers import BertModel
 
+
 class BertClassifier(nn.Module):
 
-    def __init__(self, dropout=0.2):
-
+    def __init__(self, dropout=0.3):
         super(BertClassifier, self).__init__()
 
         self.bert = BertModel.from_pretrained('bert-base-cased')
         self.dropout = nn.Dropout(dropout)
-        self.linear = nn.Linear(768, 2)
-        self.gelu = nn.GELU()
+        self.linear1 = nn.Linear(768, 384)
+        self.gelu1 = nn.GELU()
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(384, 2)
+        self.gelu2 = nn.GELU()
 
     def forward(self, input_id, mask):
-
-        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        _, pooled_output = self.bert(input_ids=input_id, attention_mask=mask, return_dict=False)
         dropout_output = self.dropout(pooled_output)
-        linear_output = self.linear(dropout_output)
-        final_layer = self.gelu(linear_output)
+        linear_output1 = self.linear1(dropout_output)
+        first_layer = self.dropout(self.gelu1(linear_output1))
+        linear_output = self.linear2(first_layer)
+        final_layer = self.gelu2(linear_output)
 
         return final_layer
 
-from torch.optim import Adam
+
+from torch.optim import AdamW
 from tqdm import tqdm
 
-def train(model, train_data, val_data, learning_rate, epochs):
 
+def train(model, train_data, val_data, learning_rate, epochs):
     train, val = Dataset(train_data), Dataset(val_data)
 
     train_dataloader = torch.utils.data.DataLoader(train, batch_size=8, shuffle=True)
@@ -147,69 +161,67 @@ def train(model, train_data, val_data, learning_rate, epochs):
     device = torch.device("cuda" if use_cuda else "cpu")
     print(f"Training on {device}")
     criterion = nn.CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr= learning_rate)
+    optimizer = AdamW(model.parameters(), lr=learning_rate)
 
     if use_cuda:
-
-            model = model.cuda()
-            criterion = criterion.cuda()
+        model = model.cuda()
+        criterion = criterion.cuda()
 
     for epoch_num in range(epochs):
 
-            total_acc_train = 0
-            total_loss_train = 0
+        total_acc_train = 0
+        total_loss_train = 0
 
-            for train_input, train_label in tqdm(train_dataloader):
+        for train_input, train_label in tqdm(train_dataloader):
+            train_label = train_label.to(device)
+            mask = train_input['attention_mask'].to(device)
+            input_id = train_input['input_ids'].squeeze(1).to(device)
 
-                train_label = train_label.to(device)
-                mask = train_input['attention_mask'].to(device)
-                input_id = train_input['input_ids'].squeeze(1).to(device)
+            output = model(input_id, mask)
+
+            batch_loss = criterion(output, train_label.long())
+            total_loss_train += batch_loss.item()
+
+            acc = (output.argmax(dim=1) == train_label).sum().item()
+            total_acc_train += acc
+
+            model.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+
+        total_acc_val = 0
+        total_loss_val = 0
+
+        with torch.no_grad():
+
+            for val_input, val_label in val_dataloader:
+                val_label = val_label.to(device)
+                mask = val_input['attention_mask'].to(device)
+                input_id = val_input['input_ids'].squeeze(1).to(device)
 
                 output = model(input_id, mask)
-                
-                batch_loss = criterion(output, train_label.long())
-                total_loss_train += batch_loss.item()
-                
-                acc = (output.argmax(dim=1) == train_label).sum().item()
-                total_acc_train += acc
 
-                model.zero_grad()
-                batch_loss.backward()
-                optimizer.step()
-            
-            total_acc_val = 0
-            total_loss_val = 0
+                batch_loss = criterion(output, val_label.long())
+                total_loss_val += batch_loss.item()
 
-            with torch.no_grad():
+                acc = (output.argmax(dim=1) == val_label).sum().item()
+                total_acc_val += acc
 
-                for val_input, val_label in val_dataloader:
-
-                    val_label = val_label.to(device)
-                    mask = val_input['attention_mask'].to(device)
-                    input_id = val_input['input_ids'].squeeze(1).to(device)
-
-                    output = model(input_id, mask)
-
-                    batch_loss = criterion(output, val_label.long())
-                    total_loss_val += batch_loss.item()
-                    
-                    acc = (output.argmax(dim=1) == val_label).sum().item()
-                    total_acc_val += acc
-            
-            print(
-                f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
+        print(
+            f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
                 | Train Accuracy: {total_acc_train / len(train_data): .3f} \
                 | Val Loss: {total_loss_val / len(val_data): .3f} \
                 | Val Accuracy: {total_acc_val / len(val_data): .3f}')
-                  
+
+
 EPOCHS = 4
 model = BertClassifier()
 LR = 1e-6
-              
+
 train(model, df_train, df_val, LR, EPOCHS)
 
-def evaluate(model, test_data):
 
+def evaluate(model, test_data):
     test = Dataset(test_data)
 
     test_dataloader = torch.utils.data.DataLoader(test, batch_size=8)
@@ -218,33 +230,22 @@ def evaluate(model, test_data):
     device = torch.device("cuda" if use_cuda else "cpu")
     print(f"Testing on {device}")
     if use_cuda:
-
         model = model.cuda()
 
     total_acc_test = 0
     with torch.no_grad():
 
         for test_input, test_label in test_dataloader:
+            test_label = test_label.to(device)
+            mask = test_input['attention_mask'].to(device)
+            input_id = test_input['input_ids'].squeeze(1).to(device)
 
-              test_label = test_label.to(device)
-              mask = test_input['attention_mask'].to(device)
-              input_id = test_input['input_ids'].squeeze(1).to(device)
+            output = model(input_id, mask)
 
-              output = model(input_id, mask)
+            acc = (output.argmax(dim=1) == test_label).sum().item()
+            total_acc_test += acc
 
-              acc = (output.argmax(dim=1) == test_label).sum().item()
-              total_acc_test += acc
-    
     print(f'Test Accuracy: {total_acc_test / len(test_data): .3f}')
-    
+
+
 evaluate(model, df_test)
-
-
-
-
-
-
-
-
-
-
---
+++ text_summarization_t5.py
@@ -0,0 +1,253 @@
+# from torch import nn
+# from transformers import BartForConditionalGeneration, BartTokenizerFast
+# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments
+# from transformers import Seq2SeqTrainer, DataCollatorForSeq2Seq
+# from torch.utils.data import DataLoader
+import nltk
+import pandas as pd
+import os
+from datasets import Dataset, DatasetDict
+from nltk import sent_tokenize
+# from datasets import DatasetDict, Dataset
+# import rouge_score
+import evaluate
+from torch.utils.data import DataLoader
+from torch.optim import AdamW
+from accelerate import Accelerator
+from transformers import get_scheduler
+from tqdm.auto import tqdm
+import torch
+import numpy as np
+
+# %%--------------------------------------------------------------------------------------------------------------------
+from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, DataCollatorForSeq2Seq, Seq2SeqTrainer, \
+    AutoTokenizer
+
+# BATCH_SIZE = 32
+body_words_cutoff = 30
+model_checkpoint = "JulesBelveze/t5-small-headline-generator"
+input_var = 'body'
+target_var = 'headline'
+splits = {'train': 0.8, 'val': 0.1, 'test': 0.1}
+random_seed = 42
+batch_size = 16
+num_train_epochs = 8
+
+
+# %%--------------------------------------------------------------------------------------------------------------------
+def filter_train_indices(data):
+    train_idx = [len(x) > body_words_cutoff for x in data.body.str.split()]
+    # np.percentile([len(x) for x in train_val_df.body.str.split()], 1)  # 57
+    return np.array(train_idx)
+
+
+code_dir = os.getcwd()
+data_dir = os.path.join(os.path.split(code_dir)[0], '')
+df = pd.read_json('Data/sarcastic_output.json')
+# df = df[:50]
+# print(df.columns)
+df = df[['body', 'headline']]
+train_val_df = df[filter_train_indices(df)]
+test_df = df[~filter_train_indices(df)]
+max_input_length = int(np.percentile([len(x) for x in train_val_df[input_var].str.split()], 99.5))
+max_target_length = int(np.percentile([len(x) for x in train_val_df[target_var].str.split()], 99.5))
+# max_target_length = 10
+print(max_target_length)
+# %%--------------------------------------------------------------------------------------------------------------------
+dataset = DatasetDict()
+train_val_df = train_val_df.sample(frac=1, random_state=random_seed).reset_index()
+total_pct = 0
+for split, pct in splits.items():
+    idx_range = (int(total_pct * len(train_val_df)), int((total_pct + pct) * len(train_val_df)))
+    total_pct = total_pct + pct
+    dataset[split] = Dataset.from_pandas(train_val_df.iloc[idx_range[0]:idx_range[1]])
+
+
+def show_samples(data, num_samples=3, seed=random_seed):
+    sample = data["train"].shuffle(seed=seed).select(range(num_samples))
+    for example in sample:
+        print(f"\n'>> Headline: {example[target_var]}'")
+        print(f"'>> Body: {example[input_var]}'")
+
+
+show_samples(dataset)
+# %%--------------------------------------------------------------------------------------------------------------------
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+
+
+# %%--------------------------------------------------------------------------------------------------------------------
+def preprocess_function(examples):
+    model_inputs = tokenizer(
+        examples[input_var],
+        max_length=max_input_length,
+        truncation=True
+    )
+    labels = tokenizer(
+        examples[target_var], max_length=max_target_length, truncation=True
+    )
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
+
+
+print("Tokenizing datasets")
+tokenized_datasets = dataset.map(preprocess_function, batched=True)
+
+# %%--------------------------------------------------------------------------------------------------------------------
+# Fine-tuning
+# Convert the dataloader into torch
+
+tokenized_datasets.set_format("torch")
+# Insantiate the model from cache
+model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint)
+data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+tokenized_datasets = tokenized_datasets.remove_columns(
+    dataset["train"].column_names
+)
+features = [tokenized_datasets["train"][i] for i in range(2)]
+data_collator(features)
+rouge_score = evaluate.load("rouge")
+# Define our own dataloader
+# batch_size = 8
+train_dataloader = DataLoader(
+    tokenized_datasets["train"],
+    shuffle=True,
+    collate_fn=data_collator,
+    batch_size=batch_size,
+)
+eval_dataloader = DataLoader(
+    tokenized_datasets["val"], collate_fn=data_collator, batch_size=batch_size
+)
+
+# Define the optimizer AdamW
+optimizer = AdamW(model.parameters(), lr=2e-5)
+
+# Now Prepare the dataset accelator prepare method
+
+accelerator = Accelerator()
+model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(
+    model, optimizer, train_dataloader, eval_dataloader
+)
+
+# Define the learning rate schedule.
+# Implement a function to post-process the summaries for evaluation.
+# Create a repository on the Hub that we can push our model to.
+# For the learning rate schedule, we’ll use the standard linear
+
+# num_train_epochs = 5
+num_update_steps_per_epoch = len(train_dataloader)
+num_training_steps = num_train_epochs * num_update_steps_per_epoch
+
+lr_scheduler = get_scheduler(
+    "linear",
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=num_training_steps,
+)
+
+
+# This method is generated summaries into sentences that are sperated by newlines.
+# This is the format the ROUGE metric expects
+
+def postprocess_text(preds, labels):
+    preds = [pred.strip() for pred in preds]
+    labels = [label.strip() for label in labels]
+
+    # ROUGE expects a newline after each sentence
+    preds = ["\n".join(nltk.sent_tokenize(pred)) for pred in preds]
+    labels = ["\n".join(nltk.sent_tokenize(label)) for label in labels]
+
+    return preds, labels
+
+
+# Save the model into 'savedmodel' folder
+output_dir = 'savedmodel/small-t5'
+#
+progress_bar = tqdm(range(num_training_steps))
+
+for epoch in range(num_train_epochs):
+    # Training
+    model.train()
+    for step, batch in enumerate(train_dataloader):
+        outputs = model(**batch)
+        loss = outputs.loss
+        accelerator.backward(loss)
+
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+        progress_bar.update(1)
+
+    # Evaluation
+    model.eval()
+    for step, batch in enumerate(eval_dataloader):
+        with torch.no_grad():
+            generated_tokens = accelerator.unwrap_model(model).generate(
+                batch["input_ids"],
+                attention_mask=batch["attention_mask"],
+            )
+
+            generated_tokens = accelerator.pad_across_processes(
+                generated_tokens, dim=1, pad_index=tokenizer.pad_token_id
+            )
+            labels = batch["labels"]
+
+            # If we did not pad to max length, we need to pad the labels too
+            labels = accelerator.pad_across_processes(
+                batch["labels"], dim=1, pad_index=tokenizer.pad_token_id
+            )
+
+            generated_tokens = accelerator.gather(generated_tokens).cpu().numpy()
+            labels = accelerator.gather(labels).cpu().numpy()
+
+            # Replace -100 in the labels as we can't decode them
+            labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+            if isinstance(generated_tokens, tuple):
+                generated_tokens = generated_tokens[0]
+            decoded_preds = tokenizer.batch_decode(
+                generated_tokens, skip_special_tokens=True
+            )
+
+            decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+            # print(decoded_preds)
+            # print(decoded_labels)
+            decoded_preds, decoded_labels = postprocess_text(
+                decoded_preds, decoded_labels
+            )
+
+            rouge_score.add_batch(predictions=decoded_preds, references=decoded_labels)
+
+    # Compute metrics
+    result = rouge_score.compute()
+    # print(result.items())
+    # Extract the median ROUGE scores
+    # result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
+    result = {k: round(v, 4) for k, v in result.items()}
+    print(f"Epoch {epoch}:", result)
+
+    # Save and upload
+    accelerator.wait_for_everyone()
+    unwrapped_model = accelerator.unwrap_model(model)
+    unwrapped_model.save_pretrained(output_dir, save_function=accelerator.save)
+    if accelerator.is_main_process:
+        tokenizer.save_pretrained(output_dir)
+
+from transformers import pipeline
+
+savedmodel = "savedmodel/small-t5"
+summarizer = pipeline("summarization", model=savedmodel)
+
+# print(dataset['test'][1])
+
+
+def print_summary(idx):
+    print(f'\n-- Prediction {i} --')
+    review = dataset['test'][idx]["body"]
+    title = dataset["test"][idx]["headline"]
+    summary = summarizer(dataset["test"][idx]["body"],min_length=10, max_length=max_target_length)[0]["summary_text"]
+    print(f"'>>> Article: {review}'")
+    print(f"\n'>>> Headline: {title}'")
+    print(f"\n'>>> Summary: {summary}'")
+
+
+for i in range(len(dataset['test'][:3])):
+    print_summary(i)
